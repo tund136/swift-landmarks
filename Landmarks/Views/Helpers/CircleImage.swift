a9b12bdac21693fe5acd7b4ab8512f2740c6a33d
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-30 14:06:09 +0700

    Create a Custom Image View

---
+++ Landmarks/Views/Helpers/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Danh Tu on 30/09/2021.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(.white, lineWidth: 4)
+            )
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
