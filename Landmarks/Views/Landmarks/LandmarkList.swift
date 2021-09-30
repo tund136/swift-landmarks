ca420672f36369173d2a19a110ca0384eab17811
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-30 15:09:07 +0700

    Create the List of Landmarks

---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Danh Tu on 30/09/2021.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
