d26143cfea5f753a667b19f37300dad50abd7b9c
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-30 15:28:33 +0700

    Make the List Dynamic

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -9,24 +9,26 @@ import Foundation
 import SwiftUI
 import CoreLocation
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     var park: String
     var state: String
     var description: String
-    
+
     private var imageName: String
     var image: Image {
         Image(imageName)
     }
-    
-    private var coodinates: Coodinates
+
+    private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
-        CLLocationCoordinate2D(latitude: coodinates.latitude, longitude: coodinates.longitude)
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
     }
-    
-    struct Coodinates: Hashable, Codable {
+
+    struct Coordinates: Hashable, Codable {
         var latitude: Double
         var longitude: Double
     }
