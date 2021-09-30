f069d30ef59bf32c6fefa13a2fd4fca5e229b80d
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-30 14:39:05 +0700

    Create a Landmark Model

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,33 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Danh Tu on 30/09/2021.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coodinates: Coodinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coodinates.latitude, longitude: coodinates.longitude)
+    }
+    
+    struct Coodinates {
+        var latitude: Double
+        var longitude: Double
+    }
+}
