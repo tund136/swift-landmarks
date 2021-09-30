7b230a217b9a2322772c495f810640cb5522030a
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-30 11:11:59 +0700

    Create the project

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Danh Tu on 30/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
