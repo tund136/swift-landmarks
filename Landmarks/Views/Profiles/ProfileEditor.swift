c71fecd85c82b3a2db5e1db5e7ec404f66d5dd0a
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-04 02:55:40 +0700

    Create a new view ProfileEditor and include a binding to the draft copy of the user's profile

---
+++ Landmarks/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by Danh Tu on 04/10/2021.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+    
+    var body: some View {
+        List {
+            HStack {
+                Text("Username").bold()
+                Divider()
+                TextField("Username", text: $profile.username)
+            }
+        }
+    }
+}
+
+struct ProfileEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditor(profile: .constant(.default))
+    }
+}
