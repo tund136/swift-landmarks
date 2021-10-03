7639a436185b1e119f487528aba7418ca16f3f17
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-03 11:24:08 +0700

    Add data for the six segments, one for each side of the hexagon

--- Landmarks/Views/Landmarks/HexagonParameters.swift
+++ Landmarks/Views/Landmarks/HexagonParameters.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import CoreGraphics
+
+struct HexagonParameters {
+    struct Segment {
+        var line: CGPoint
+        var curve: CGPoint
+        var control: CGPoint
+    }
+    
+    static let segment = [
+        Segment(
+            line:    CGPoint(x: 0.60, y: 0.05),
+            curve:   CGPoint(x: 0.40, y: 0.05),
+            control: CGPoint(x: 0.50, y: 0.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.05, y: 0.20),
+            curve:   CGPoint(x: 0.00, y: 0.30),
+            control: CGPoint(x: 0.00, y: 0.25)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.00, y: 0.70),
+            curve:   CGPoint(x: 0.05, y: 0.80),
+            control: CGPoint(x: 0.00, y: 0.75)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.40, y: 0.95),
+            curve:   CGPoint(x: 0.60, y: 0.95),
+            control: CGPoint(x: 0.50, y: 1.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.95, y: 0.80),
+            curve:   CGPoint(x: 1.00, y: 0.70),
+            control: CGPoint(x: 1.00, y: 0.75)
+        ),
+        Segment(
+            line:    CGPoint(x: 1.00, y: 0.30),
+            curve:   CGPoint(x: 0.95, y: 0.20),
+            control: CGPoint(x: 1.00, y: 0.25)
+        )
+    ]
+}
