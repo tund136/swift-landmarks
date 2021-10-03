66e726514da37d727ccc4215203180d95163958a
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-03 16:18:21 +0700

    Define a new ripple animation and apply it to each generated graph capsule, along with a slide transition

--- Landmarks/Views/Hikes/HikeGraph.swift
+++ Landmarks/Views/Hikes/HikeGraph.swift
@@ -1,16 +1,22 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-The elevation, heart rate, and pace of a hike plotted on a graph.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ The elevation, heart rate, and pace of a hike plotted on a graph.
+ */
 
 import SwiftUI
+
+extension Animation {
+    static func ripple() -> Animation {
+        Animation.default
+    }
+}
 
 struct HikeGraph: View {
     var hike: Hike
     var path: KeyPath<Hike.Observation, Range<Double>>
-
+    
     var color: Color {
         switch path {
         case \.elevation:
@@ -23,13 +29,13 @@ struct HikeGraph: View {
             return .black
         }
     }
-
+    
     var body: some View {
         let data = hike.observations
         let overallRange = rangeOfRanges(data.lazy.map { $0[keyPath: path] })
         let maxMagnitude = data.map { magnitude(of: $0[keyPath: path]) }.max()!
         let heightRatio = 1 - CGFloat(maxMagnitude / magnitude(of: overallRange))
-
+        
         return GeometryReader { proxy in
             HStack(alignment: .bottom, spacing: proxy.size.width / 120) {
                 ForEach(Array(data.enumerated()), id: \.offset) { index, observation in
@@ -39,6 +45,8 @@ struct HikeGraph: View {
                         range: observation[keyPath: path],
                         overallRange: overallRange)
                         .colorMultiply(color)
+                        .transition(.slide)
+                        .animation(.ripple())
                 }
                 .offset(x: 0, y: proxy.size.height * heightRatio)
             }
@@ -47,7 +55,7 @@ struct HikeGraph: View {
 }
 
 func rangeOfRanges<C: Collection>(_ ranges: C) -> Range<Double>
-    where C.Element == Range<Double> {
+where C.Element == Range<Double> {
     guard !ranges.isEmpty else { return 0..<0 }
     let low = ranges.lazy.map { $0.lowerBound }.min()!
     let high = ranges.lazy.map { $0.upperBound }.max()!
@@ -60,7 +68,7 @@ func magnitude(of range: Range<Double>) -> Double {
 
 struct HikeGraph_Previews: PreviewProvider {
     static var hike = ModelData().hikes[0]
-
+    
     static var previews: some View {
         Group {
             HikeGraph(hike: hike, path: \.elevation)
