58063ce500c91f5f344fd0c4325ee21fc6fad189
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-04 03:03:02 +0700

    Add a cancel button to ProfileHost

--- Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -15,6 +15,12 @@ struct ProfileHost: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             HStack {
+                if editMode?.wrappedValue == .active {
+                    Button("Cancel") {
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
                 Spacer()
                 EditButton()
             }
