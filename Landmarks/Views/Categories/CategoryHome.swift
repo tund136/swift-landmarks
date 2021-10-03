162ab20e24048a4210a72a03e33a19912865c4b0
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-04 02:36:26 +0700

    Add the listStyle modifier and apply Inset ListStyle to remove the insets.

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -26,6 +26,7 @@ struct CategoryHome: View {
                 }
                 .listRowInsets(EdgeInsets())
             }
+            .listStyle(InsetListStyle())
             .navigationTitle("Featured")
             .toolbar {
                 Button(action: { showingProfile.toggle() }) {
