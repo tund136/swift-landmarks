639b4d1a5f3de9766e288ecc2042165422a8aedb
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-04 02:15:21 +0700

    Add a user profile button to the navigation bar using the toolbar modifier

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
     
     var body: some View {
         NavigationView {
@@ -26,6 +27,16 @@ struct CategoryHome: View {
                 .listRowInsets(EdgeInsets())
             }
             .navigationTitle("Featured")
+            .toolbar {
+                Button(action: { showingProfile.toggle() }) {
+                    Image(systemName: "person.crop.circle")
+                        .accessibilityLabel("User Profile")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelData)
+            }
         }
     }
 }
