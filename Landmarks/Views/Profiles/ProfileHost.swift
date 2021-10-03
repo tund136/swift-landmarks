60e5ffcad5f260dbb1f828212e9e07f9d250fb58
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-04 01:50:30 +0700

    Add a view to that group with a text view that displays the username of a stored profile

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Danh Tu on 04/10/2021.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        Text("Profile for: \(draftProfile.username)")
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
