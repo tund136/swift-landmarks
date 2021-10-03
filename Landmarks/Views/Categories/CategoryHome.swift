a7c1051a2aa1a09f87bffb5d5232bdd940bf9ab5
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-03 16:29:16 +0700

    Add a Category View

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Danh Tu on 03/10/2021.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
