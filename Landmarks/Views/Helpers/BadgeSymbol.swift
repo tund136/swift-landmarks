057357ebf8a231d2d60cea6790027ee3d5a35ecf
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-03 15:30:15 +0700

    Create BadgeSymbol for the mountain shape

---
+++ Landmarks/Views/Helpers/BadgeSymbol.swift
@@ -0,0 +1,20 @@
+//
+//  BadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Danh Tu on 03/10/2021.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
