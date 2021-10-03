d51ecb2fdba037a2480529a23842b75ea978cb91
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-04 03:08:36 +0700

    Apply modifiers to populate the editor

--- Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -29,6 +29,12 @@ struct ProfileHost: View {
                 ProfileSummary(profile: modelData.profile)
             } else {
                 ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear {
+                        modelData.profile = draftProfile
+                    }
             }
         }
         .padding()
