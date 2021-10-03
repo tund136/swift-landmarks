4768b2698502c3cc558668477f11c40255f79acd
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-03 15:06:12 +0700

    Add an adjustment value that lets you tune the shape of the hexagon

--- Landmarks/Views/Landmarks/HexagonParameters.swift
+++ Landmarks/Views/Landmarks/HexagonParameters.swift
@@ -15,6 +15,8 @@ struct HexagonParameters {
         var control: CGPoint
     }
     
+    static let adjustment: CGFloat = 0.085
+    
     static let segment = [
         Segment(
             line:    CGPoint(x: 0.60, y: 0.05),
@@ -22,14 +24,14 @@ struct HexagonParameters {
             control: CGPoint(x: 0.50, y: 0.00)
         ),
         Segment(
-            line:    CGPoint(x: 0.05, y: 0.20),
-            curve:   CGPoint(x: 0.00, y: 0.30),
-            control: CGPoint(x: 0.00, y: 0.25)
+            line:    CGPoint(x: 0.05, y: 0.20 + adjustment),
+            curve:   CGPoint(x: 0.00, y: 0.30 + adjustment),
+            control: CGPoint(x: 0.00, y: 0.25 + adjustment)
         ),
         Segment(
-            line:    CGPoint(x: 0.00, y: 0.70),
-            curve:   CGPoint(x: 0.05, y: 0.80),
-            control: CGPoint(x: 0.00, y: 0.75)
+            line:    CGPoint(x: 0.00, y: 0.70 - adjustment),
+            curve:   CGPoint(x: 0.05, y: 0.80 - adjustment),
+            control: CGPoint(x: 0.00, y: 0.75 - adjustment)
         ),
         Segment(
             line:    CGPoint(x: 0.40, y: 0.95),
@@ -37,14 +39,14 @@ struct HexagonParameters {
             control: CGPoint(x: 0.50, y: 1.00)
         ),
         Segment(
-            line:    CGPoint(x: 0.95, y: 0.80),
-            curve:   CGPoint(x: 1.00, y: 0.70),
-            control: CGPoint(x: 1.00, y: 0.75)
+            line:    CGPoint(x: 0.95, y: 0.80 - adjustment),
+            curve:   CGPoint(x: 1.00, y: 0.70 - adjustment),
+            control: CGPoint(x: 1.00, y: 0.75 - adjustment)
         ),
         Segment(
-            line:    CGPoint(x: 1.00, y: 0.30),
-            curve:   CGPoint(x: 0.95, y: 0.20),
-            control: CGPoint(x: 1.00, y: 0.25)
+            line:    CGPoint(x: 1.00, y: 0.30 + adjustment),
+            curve:   CGPoint(x: 0.95, y: 0.20 + adjustment),
+            control: CGPoint(x: 1.00, y: 0.25 + adjustment)
         )
     ]
 }
