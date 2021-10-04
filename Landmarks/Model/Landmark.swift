8c16e965d99b6ff55e99084871d0379d4e78974c
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-04 23:10:18 +0700

    Return the feature image, if it exists

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -25,19 +25,23 @@ struct Landmark: Hashable, Codable, Identifiable {
         case rivers = "Rivers"
         case mountains = "Mountains"
     }
-
+    
     private var imageName: String
     var image: Image {
         Image(imageName)
     }
-
+    
+    var featureImage: Image? {
+        isFeatured ? Image(imageName + "_feature") : nil
+    }
+    
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
             latitude: coordinates.latitude,
             longitude: coordinates.longitude)
     }
-
+    
     struct Coordinates: Hashable, Codable {
         var latitude: Double
         var longitude: Double
