2a8b233ad9d6db02482668de262a0a5dc9a42418
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-04 23:24:46 +0700

    Overlay text information about the landmark on the image

--- Landmarks/Views/Landmarks/FeatureCard.swift
+++ Landmarks/Views/Landmarks/FeatureCard.swift
@@ -14,6 +14,39 @@ struct FeatureCard: View {
         landmark.featureImage?
             .resizable()
             .aspectRatio(3 / 2, contentMode: .fit)
+            .overlay(TextOverlay(landmark: landmark))
+    }
+}
+
+struct TextOverlay: View {
+    var landmark: Landmark
+    
+    var gradient: LinearGradient {
+        LinearGradient(
+            gradient: Gradient(
+                colors: [
+                    Color.black.opacity(0.6),
+                    Color.black.opacity(0)
+                ]
+            ),
+            startPoint: .bottom,
+            endPoint: .center)
+    }
+    
+    var body: some View {
+        ZStack(alignment: .bottomLeading) {
+            Rectangle().fill(gradient)
+            
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                    .bold()
+                
+                Text(landmark.park)
+            }
+            .padding()
+        }
+        .foregroundColor(.white)
     }
 }
 
